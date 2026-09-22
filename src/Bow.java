e2a5888a3631f6095f2ff76e80720e32bae5818f
user <254574@astanait.edu.kz>
2026-09-22 10:56:57 +0500

    Commit 1: Product(Weapon),ConcreteProducts(Sword & Bow)

---
+++ src/Bow.java
@@ -0,0 +1,6 @@
+public class Bow implements Weapon{
+    @Override
+    public void use(){
+        System.out.println("Shooting an arrow from a long bow!");
+    }
+}
---
+++ src/Sword.java
@@ -0,0 +1,6 @@
+public class Sword implements Weapon{
+    @Override
+    public void use(){
+        System.out.println("Striking with a sharp steel sword!");
+    }
+}
